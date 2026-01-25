dc03891278428c40fa7be42a036dce748a520217
Athiief <aziiemuth@gmail.com>
2026-01-25 15:20:15 +0700

    Add script to clean tracking parameters (fbclid/utm) from URL

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import GlobalStyles from './styles/GlobalStyles';
 import Navbar from './components/Navbar';
@@ -16,6 +17,28 @@ import WhatsAppFloat from './components/WhatsAppFloat';
 import ScrollToTop from './components/ui/ScrollToTop';
 
 export default function Home() {
+  // Script untuk membersihkan URL dari tracking parameters (IG/FB)
+  useEffect(function() {
+    if (typeof window !== 'undefined' && window.location.search) {
+      var url = new URL(window.location.href);
+      var params = new URLSearchParams(url.search);
+      var paramsToRemove = ['fbclid', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'];
+      var changed = false;
+
+      Array.from(params.keys()).forEach(function(key) {
+        if (paramsToRemove.includes(key) || key.startsWith('utm_')) {
+          params.delete(key);
+          changed = true;
+        }
+      });
+
+      if (changed) {
+        var newUrl = params.toString() ? window.location.pathname + '?' + params.toString() : window.location.pathname;
+        window.history.replaceState({}, '', newUrl);
+      }
+    }
+  }, []);
+
   return (
     <ThemeProvider>
       <GlobalStyles />
